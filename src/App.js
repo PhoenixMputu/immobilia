1467ce781144fc27b1de79503293d4670b399130
“Victory <Victory23tshimbadi@gmail.com>
2023-01-02 15:11:31 +0100

    add login and register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './compenents/Login';
+import Home from './compenents/Home';
+import Register from './compenents/Register';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" index element={<Login />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/login" index element={<Login />} />
+				<Route path="/register" index element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	);
