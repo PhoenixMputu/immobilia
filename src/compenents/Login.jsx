d8b0f8afff6289acf2b7584aae9cc0824c06d2d8
“Victory <Victory23tshimbadi@gmail.com>
2023-01-02 16:27:41 +0100

    add validation

--- src/compenents/Login.jsx
+++ src/compenents/Login.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Link, redirect } from 'react-router-dom';
 
 const Login = () => {
+    const [data, setData] = useState({
+        email: '',
+        password: ''
+    })
+
     const currentUser = localStorage.getItem('userData');
     if (currentUser) {
         return redirect('/')
     }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setData((prevState) => ({...prevState, [name]: value}));
+        console.log(data);
+    }
+    
   return (
     <div className='container__login'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Connexion</h1>
             <div className='group-input'>
                 <label>Email</label>
-                <input type="email" required/>
+                <input type="email" name='email' required/>
             </div>
             <div className='group-input'>
                 <label>Mot de passe</label>
-                <input type="passwoord" required/>
+                <input type="passwoord" name='password' required/>
             </div>
             <div className='container__links'>
                 <Link to="/forget">Mot de passe oublié</Link>
--- src/compenents/Register.jsx
+++ src/compenents/Register.jsx
@@ -1,48 +1,74 @@
 import React, {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Link, redirect } from 'react-router-dom'
 
 const Register = () => {
     const [data, setData] = useState({
         name: '',
-        firstname: '',
+        firstName: '',
         email: '',
         password: '',
         confirmPassword: '',
-    })
+    });
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (data.name.length < 3) {
+            toast.error('Le nom doit avoir au moins 3 caractères', {
+                position: toast.POSITION.TOP_RIGHT
+            })
+        }
+        
+        if (data.firstName.length < 3) {
+            toast.error('Le prénom doit avoir au moins 3 caractères', {
+                position: toast.POSITION.TOP_RIGHT
+            })
+        }
+        
+        if (data.password === data.confirmPassword) {
+            toast.error('Les mots de passe ne sont pas identique', {
+                position: toast.POSITION.TOP_RIGHT
+            })
+        }
+    }
+    
     const handleChange = (e) => {
         const {name, value} = e.target
         setData((prevState) => ({...prevState, [name]: value}));
+        console.log(data);
     }
+
   return (
     <div className='container__login'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h1>Inscription</h1>
             <div className='group-input'>
                 <label>Nom</label>
-                <input type="email" onChange={handleChange} required/>
+                <input type="text" name='name' onChange={handleChange} required/>
             </div>
             <div className='group-input'>
                 <label>Prénom</label>
-                <input type="email" onChange={handleChange} required/>
+                <input type="text" name='firstName' onChange={handleChange} required/>
             </div>
             <div className='group-input'>
                 <label>Email</label>
-                <input type="email" onChange={handleChange} required/>
+                <input type="email" name='email' onChange={handleChange} required/>
             </div>
             <div className='group-input'>
                 <label>Mot de passe</label>
-                <input type="passwoord" onChange={handleChange} required/>
+                <input type="passwoord" name='password' onChange={handleChange} required/>
             </div>
             <div className='group-input'>
                 <label>Confirmation</label>
-                <input type="passwoord" onChange={handleChange} required/>
+                <input type="passwoord" name='confirmPassword' onChange={handleChange} required/>
             </div>
             <input type="submit" className='btn-primary mt' value="S'inscrire" />
             <div className='container__links'>
                 <Link to="/login">J'ai déjà un compte ?</Link>
             </div>
         </form>
+        <ToastContainer />
     </div>
   )
 }
