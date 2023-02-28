85d635661967b7aceaa52ad61d63922075cc2913
“Victory <victory23tshimbadi@gmail.com>
2023-02-28 21:07:08 +0100

    test commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" index element={<Login />} />
-				<Route path="/register" index element={<Register />} />
+				<Route path="/register" element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	);
--- src/compenents/Home.jsx
+++ src/compenents/Home.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import { redirect } from 'react-router-dom';
 
 const Home = () => {
-    const currentUser = localStorage.getItem('userData');
-    console.log(currentUser);
-    if (!currentUser) {
-        return redirect('http://localhost:3000/login')
-    }
   return (
     <div>Home Page</div>
   )
